f23a2fd07970c008c97fa56b56d9118b9b4aa9fb
Sumit prasad <sumitprasadd.wb24@gmail.com>
2026-04-04 23:36:05 +0530

    update exception handling

--- app/middlewares/logging.py
+++ app/middlewares/logging.py
@@ -6,12 +6,18 @@ from app.utils.logger import logger
 
 class LoggingMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
-        start = time.time()
+        try:
+            start = time.time()
 
-        response = await call_next(request)
+            response = await call_next(request)
 
-        duration = time.time() - start
+            duration = time.time() - start
 
-        logger.info(f"{request.method} {request.url} - {duration:.4f}s")
+            logger.info(f"{request.method} {request.url} - {duration:.4f}s")
 
-        return response
+            return response
+        
+        except Exception as e:
+            logger.info(f"Error: {e}")
+            print(f"[ERROR] {e}")
+            raise e
